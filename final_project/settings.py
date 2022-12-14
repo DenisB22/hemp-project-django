c7aeba7e21dc327863b9c7d0d4810a7031154542
DenisB22 <bechiragich97@gmail.com>
2022-12-14 22:18:06 +0200

    fixed

--- final_project/settings.py
+++ final_project/settings.py
@@ -188,5 +188,5 @@ EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = bool(os.environ.get('EMAIL_USE_TLS'))
 # EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
 
