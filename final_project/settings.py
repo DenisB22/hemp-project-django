5d4753da882b3a83227d296a22ace14859b57e1b
DenisB22 <bechiragich97@gmail.com>
2022-12-14 16:35:48 +0200

    Update settings.py

--- final_project/settings.py
+++ final_project/settings.py
@@ -104,7 +104,7 @@ AUTH_USER_MODEL = 'accounts.Account'
 DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE'),
-        'NAME': config('DB_NAME'),
+        'NAME': os.getenv('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST'),
